dc5bc6ba826c53b1d0496189203ee01a663d3f23
roachmanza <roche.olivier@outlook.com>
2017-07-05 01:55:47 +0200

    Updated table creation

--- _Database/PostGres/2_Create_Tables.sql
+++ _Database/PostGres/2_Create_Tables.sql
@@ -1,60 +1,22 @@
 
 
---Groups define the process of people grouped together , in teams department or company.
-CREATE TABLE public."GroupCss"
-(
-    "GroupCssId" integer NOT NULL,
-    "Name" character varying COLLATE pg_catalog."default",
-    "CssValue" character varying COLLATE pg_catalog."default",
-    "InActiveDate" date,
-    "InActive" bit(1),
-    CONSTRAINT "GroupCss_pkey" PRIMARY KEY ("GroupCssId")
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
 
-CREATE TABLE public."GroupTypes"
-(
-    "GroupTypeId" integer NOT NULL,
-    "Name" character varying COLLATE pg_catalog."default",
-    "Description" character varying COLLATE pg_catalog."default",
-    "InActiveDate" date,
-    "InActive" bit(1),
-    CONSTRAINT "GroupTypes_pkey" PRIMARY KEY ("GroupTypeId")
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
-
-CREATE TABLE public."Groups"
-(
-    "GroupId" integer NOT NULL,
-    "GroupTypeId" integer NOT NULL,
-    "GroupCssId" integer NOT NULL,
-    "Name" character varying COLLATE pg_catalog."default",
-    "Description" character varying COLLATE pg_catalog."default",
-    "InActiveDate" date,
-    "InActive" bit(1),
-    CONSTRAINT "Groups_pkey" PRIMARY KEY ("GroupId"),
-    CONSTRAINT "Groups_GroupCss_fkey" FOREIGN KEY ("GroupCssId")
-        REFERENCES public."GroupCss" ("GroupCssId") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT "Groups_GroupTypes_fkey" FOREIGN KEY ("GroupTypeId")
-        REFERENCES public."GroupTypes" ("GroupTypeId") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
-
-
+--== PERSON DEFINITION ==--
 --Persons and person types ( Person types can be difined as permanent , temp or contractor or any other type that is needed.)
+CREATE TABLE public."EmploymentTypes"
+(
+    "EmploymentTypeId" integer NOT NULL,
+    "Name" character varying COLLATE pg_catalog."default",
+    "Description" character varying COLLATE pg_catalog."default",
+    "InActiveDate" date,
+    "InActive" bit(1),
+    CONSTRAINT "EmploymentTypes_pkey" PRIMARY KEY ("EmploymentTypeId")
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
 CREATE TABLE public."PersonTypes"
 (
     "PersonTypeId" integer NOT NULL,
@@ -63,6 +25,21 @@ CREATE TABLE public."PersonTypes"
     "InActiveDate" date,
     "InActive" bit(1),
     CONSTRAINT "PersonTypes_pkey" PRIMARY KEY ("PersonTypeId")
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+--Roles and persons in that roles
+CREATE TABLE public."Roles"
+(
+    "RoleId" integer NOT NULL,
+    "Name" character varying COLLATE pg_catalog."default",
+    "Description" character varying COLLATE pg_catalog."default",
+    "InActiveDate" date,
+    "InActive" bit(1),
+    CONSTRAINT "Roles_pkey" PRIMARY KEY ("RoleId")
 )
 WITH (
     OIDS = FALSE
@@ -90,23 +67,21 @@ WITH (
 )
 TABLESPACE pg_default;
 
-
---Group setup , persons in the group , and what types of persons can be added to the group.
---Groups gets created for groups and then the group type gets defined with roles and person types that can belong to the groups.
---This enables the system to have different roles for different person types.
-CREATE TABLE public."GroupPersons"
+CREATE TABLE public."PersonEmploymentTypes"
 (
-    "GroupPersonId" integer NOT NULL,
-    "GroupId" integer NOT NULL,
+    "PersonEmploymentTypeId" integer NOT NULL,
     "PersonId" integer NOT NULL,
+    "EmploymentTypeId" integer NOT NULL,
+    "Name" character varying COLLATE pg_catalog."default",
+    "Description" character varying COLLATE pg_catalog."default",
     "InActiveDate" date,
     "InActive" bit(1),
-    CONSTRAINT "GroupPersons_pkey" PRIMARY KEY ("GroupPersonId"),
-    CONSTRAINT "GroupPersons_Groups_fkey" FOREIGN KEY ("GroupId")
-        REFERENCES public."Groups" ("GroupId") MATCH SIMPLE
+    CONSTRAINT "PersonEmploymentTypes_pkey" PRIMARY KEY ("PersonEmploymentTypeId"),
+    CONSTRAINT "PersonEmploymentTypes_EmploymentTypes_fkey" FOREIGN KEY ("EmploymentTypeId")
+        REFERENCES public."EmploymentTypes" ("EmploymentTypeId") MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT "GroupPersons_Persons_fkey" FOREIGN KEY ("PersonId")
+    CONSTRAINT "PersonEmploymentTypes_Persons_fkey" FOREIGN KEY ("PersonId")
         REFERENCES public."Persons" ("PersonId") MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
@@ -116,20 +91,19 @@ WITH (
 )
 TABLESPACE pg_default;
 
-CREATE TABLE public."GroupGrantPersonTypes"
+CREATE TABLE public."PersonInformations"
 (
-    "GroupGrantPersonTypeId" integer NOT NULL,
-    "GroupId" integer NOT NULL,
-    "PersonTypeId" integer NOT NULL,
+    "PersonInformationId" integer NOT NULL,
+    "PersonId" integer NOT NULL,
+    "Phone" character varying COLLATE pg_catalog."default",
+    "Fax" character varying COLLATE pg_catalog."default",
+    "Email" character varying COLLATE pg_catalog."default",
+    "Address" character varying COLLATE pg_catalog."default",
     "InActiveDate" date,
     "InActive" bit(1),
-    CONSTRAINT "GroupGrantPersonTypes_pkey" PRIMARY KEY ("GroupGrantPersonTypeId"),
-    CONSTRAINT "GroupGrantPersonTypes_Groups_fkey" FOREIGN KEY ("GroupId")
-        REFERENCES public."Groups" ("GroupId") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT "GroupGrantPersonTypes_PersonTypes_fkey" FOREIGN KEY ("PersonTypeId")
-        REFERENCES public."PersonTypes" ("PersonTypeId") MATCH SIMPLE
+    CONSTRAINT "PersonInformations_pkey" PRIMARY KEY ("PersonInformationId"),
+    CONSTRAINT "PersonInformations_Persons_fkey" FOREIGN KEY ("PersonId")
+        REFERENCES public."Persons" ("PersonId") MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
@@ -138,34 +112,20 @@ WITH (
 )
 TABLESPACE pg_default;
 
---Roles and persons in that roles
-CREATE TABLE public."Roles"
-(
-    "RoleId" integer NOT NULL,
-    "Name" character varying COLLATE pg_catalog."default",
-    "Description" character varying COLLATE pg_catalog."default",
-    "InActiveDate" date,
-    "InActive" bit(1),
-    CONSTRAINT "Roles_pkey" PRIMARY KEY ("RoleId")
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
 
-CREATE TABLE public."GroupGrantRoles"
+CREATE TABLE public."PersonRoles"
 (
-    "GroupGrantRoleId" integer NOT NULL,
-    "GroupId" integer NOT NULL,
+    "PersonRoleId" integer NOT NULL,
+    "PersonId" integer NOT NULL,
     "RoleId" integer NOT NULL,
     "InActiveDate" date,
     "InActive" bit(1),
-    CONSTRAINT "GroupGrantRoles_pkey" PRIMARY KEY ("GroupGrantRoleId"),
-    CONSTRAINT "GroupGrantRoles_Groups_fkey" FOREIGN KEY ("GroupId")
-        REFERENCES public."Groups" ("GroupId") MATCH SIMPLE
+    CONSTRAINT "PersonRoles_pkey" PRIMARY KEY ("PersonRoleId"),
+    CONSTRAINT "PersonRoles_Persons_fkey" FOREIGN KEY ("PersonId")
+        REFERENCES public."Persons" ("PersonId") MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT "GroupGrantRoles_PersonTypes_fkey" FOREIGN KEY ("RoleId")
+    CONSTRAINT "PersonRoles_Roles_fkey" FOREIGN KEY ("RoleId")
         REFERENCES public."Roles" ("RoleId") MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
@@ -176,26 +136,142 @@ WITH (
 TABLESPACE pg_default;
 
 
-
-
-
-
-
-
-
-CREATE TABLE public."AwardPrograms"
+--== GROUPS DEFINITIION ==--
+--Groups define the process of people grouped together , in teams department or company.
+CREATE TABLE public."GroupTypes"
 (
-    "AwardProgramId" integer NOT NULL,
+    "GroupTypeId" integer NOT NULL,
     "Name" character varying COLLATE pg_catalog."default",
     "Description" character varying COLLATE pg_catalog."default",
+    "InActiveDate" date,
     "InActive" bit(1),
-    "InActiveDate" date,    
-    CONSTRAINT "AwardPrograms_pkey" PRIMARY KEY ("AwardProgramId")
+    CONSTRAINT "GroupTypes_pkey" PRIMARY KEY ("GroupTypeId")
 )
 WITH (
     OIDS = FALSE
 )
 TABLESPACE pg_default;
+
+CREATE TABLE public."Groups"
+(
+    "GroupId" integer NOT NULL,
+    "GroupTypeId" integer NOT NULL,
+    "Name" character varying COLLATE pg_catalog."default",
+    "Description" character varying COLLATE pg_catalog."default",
+    "InActiveDate" date,
+    "InActive" bit(1),
+    CONSTRAINT "Groups_pkey" PRIMARY KEY ("GroupId"),
+    CONSTRAINT "Groups_GroupTypes_fkey" FOREIGN KEY ("GroupTypeId")
+        REFERENCES public."GroupTypes" ("GroupTypeId") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+CREATE TABLE public."GroupOfPersons"
+(
+    "GroupOfPersonId" integer NOT NULL,
+    "GroupId" integer NOT NULL,
+    "PersonId" integer NOT NULL,
+    "InActiveDate" date,
+    "InActive" bit(1),
+    CONSTRAINT "GroupOfPersons_pkey" PRIMARY KEY ("GroupOfPersonId"),
+    CONSTRAINT "GroupOfPersons_Groups_fkey" FOREIGN KEY ("GroupId")
+        REFERENCES public."Groups" ("GroupId") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT "GroupOfPersons_Persons_fkey" FOREIGN KEY ("PersonId")
+        REFERENCES public."Persons" ("PersonId") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+
+--==BUSINESS PROGRAM ==--
+CREATE TABLE public."BusinessPrograms"
+(
+    "BusinessProgramId" integer NOT NULL,
+    "Name" character varying COLLATE pg_catalog."default",
+    "Description" character varying COLLATE pg_catalog."default",
+    "InActive" bit(1),
+    "InActiveDate" date,    
+    CONSTRAINT "BusinessPrograms_pkey" PRIMARY KEY ("BusinessProgramId")
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+CREATE TABLE public."ProgramAwardTypes"
+(
+    "ProgramAwardTypeId" integer NOT NULL,
+    "Name" character varying COLLATE pg_catalog."default",
+    "Description" character varying COLLATE pg_catalog."default",
+    "InActive" bit(1),
+    "InActiveDate" date,    
+    CONSTRAINT "ProgramAwardTypes_pkey" PRIMARY KEY ("ProgramAwardTypeId")
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+CREATE TABLE public."BusinessProgramAwardTypes"
+(
+    "BusinessProgramAwardTypeId" integer NOT NULL,
+    "BusinessProgramId" integer NOT NULL,
+    "ProgramAwardTypeId" integer NOT NULL,
+    "InActive" bit(1),
+    "InActiveDate" date,    
+    CONSTRAINT "BusinessProgramAwardTypes_pkey" PRIMARY KEY ("BusinessProgramAwardTypeId"),
+    CONSTRAINT "BusinessProgramAwardTypes_BusinessPrograms_fkey" FOREIGN KEY ("BusinessProgramId")
+        REFERENCES public."BusinessPrograms" ("BusinessProgramId") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT "BusinessProgramAwardTypes_ProgramAwardTypes_fkey" FOREIGN KEY ("ProgramAwardTypeId")
+        REFERENCES public."ProgramAwardTypes" ("ProgramAwardTypeId") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+CREATE TABLE public."BusinessProgramGroups"
+(
+    "BusinessProgramGroupId" integer NOT NULL,
+    "BusinessProgramId" integer NOT NULL,
+    "GroupId" integer NOT NULL,
+    "InActive" bit(1),
+    "InActiveDate" date,    
+    CONSTRAINT "BusinessProgramGroups_pkey" PRIMARY KEY ("BusinessProgramGroupId"),
+    CONSTRAINT "BusinessProgramGroups_BusinessPrograms_fkey" FOREIGN KEY ("BusinessProgramId")
+        REFERENCES public."BusinessPrograms" ("BusinessProgramId") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT "BusinessProgramGroups_Groups_fkey" FOREIGN KEY ("GroupId")
+        REFERENCES public."Groups" ("GroupId") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+
+
+
+
+
 
 
 CREATE TABLE public."PeriodTypes"
@@ -206,21 +282,6 @@ CREATE TABLE public."PeriodTypes"
     "InActive" bit(1),
     "InActiveDate" date,    
     CONSTRAINT "PeriodTypes_pkey" PRIMARY KEY ("PeriodTypeId")
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
-
-
-CREATE TABLE public."AwardTypes"
-(
-    "AwardTypeId" integer NOT NULL,
-    "Name" character varying COLLATE pg_catalog."default",
-    "Description" character varying COLLATE pg_catalog."default",
-    "InActive" bit(1),
-    "InActiveDate" date,    
-    CONSTRAINT "AwardTypes_pkey" PRIMARY KEY ("AwardTypeId")
 )
 WITH (
     OIDS = FALSE
--- _Database/PostGres/4_Create_BaseValues.sql
+++ _Database/PostGres/4_Create_BaseValues.sql
@@ -1,4 +1,22 @@
 
+INSERT INTO public."ProgramAwardTypes"(
+	"ProgramAwardTypeId", "Name", "Description", "InActive", "InActiveDate")
+	VALUES (1, 'Personal', 'Personal award', '0', '1900-01-01'),
+    (2, 'Group', 'Group award', '0', '1900-01-01'),
+    (3, 'Other', 'Other award', '0', '1900-01-01');
+
+INSERT INTO public."GroupTypes"(
+	"GroupTypeId", "Name", "Description", "InActive", "InActiveDate")
+	VALUES (1, 'System', 'System group', '0', '1900-01-01'),
+    (2, 'Moderator', 'System group', '0', '1900-01-01'),
+    (3, 'Individual', 'System group', '0', '1900-01-01'),
+    (4, 'Team', 'Team group', '0', '1900-01-01'),
+    (5, 'Business unit', 'Business unit group', '0', '1900-01-01'),
+    (6, 'Department', 'Department group', '0', '1900-01-01'),
+    (7, 'Cost centre', 'Cost centre group', '0', '1900-01-01'),
+    (8, 'Company', 'Company group', '0', '1900-01-01'),
+    (9, 'Adhoc', 'Adhoc group', '0', '1900-01-01'),
+    (10, 'Other', 'Adhoc group', '0', '1900-01-01');
 
 INSERT INTO public."PeriodTypes"(
 	"PeriodTypeId", "Name", "Description", "InActive", "InActiveDate")
@@ -14,33 +32,16 @@ INSERT INTO public."PeriodTypes"(
     (9, 'Adhoc', 'Adhoc award', '0', '1900-01-01'),
     (10, 'Other', 'Other award', '0', '1900-01-01');
 
-
-INSERT INTO public."GroupTypes"(
-	"GroupTypeId", "Name", "Description", "InActive", "InActiveDate")
-	VALUES (1, 'Team', 'Team group', '0', '1900-01-01'),
-    (2, 'Business unit', 'Business unit group', '0', '1900-01-01'),
-    (3, 'Department', 'Department group', '0', '1900-01-01'),
-    (4, 'Cost centre', 'Cost centre group', '0', '1900-01-01'),
-    (5, 'Company', 'Company group', '0', '1900-01-01'),
-    (6, 'Adhoc', 'Adhoc group', '0', '1900-01-01'),
-    (7, 'Other', 'Adhoc group', '0', '1900-01-01');
-
-    
-INSERT INTO public."AwardTypes"(
-	"AwardTypeId", "Name", "Description", "InActive", "InActiveDate")
-	VALUES (1, 'Personal', 'Personal award', '0', '1900-01-01'),
-    (2, 'Group', 'Group award', '0', '1900-01-01'),
-    (3, 'Other', 'Other award', '0', '1900-01-01');
-
-    
 INSERT INTO public."PersonTypes"(
 	"PersonTypeId", "Name", "Description", "InActive", "InActiveDate")
-	VALUES (1, 'Office', 'This person is a office worker', '0', '1900-01-01'),
-    (2, 'Sales', 'This person is a sales rep', '0', '1900-01-01'),
-    (3, 'Roaming', 'This person is a roaming employee', '0', '1900-01-01'),
-    (4, 'Temp', 'This person is a temp', '0', '1900-01-01'),
-    (5, 'Contractor', 'This person is a contrtactor', '0', '1900-01-01'),
-    (6, 'Other', 'This person is an other type of employee', '0', '1900-01-01');
+	VALUES
+    (1, 'Individual', 'This person is a individual', '0', '1900-01-01'), 
+    (2, 'Office', 'This person is a office worker', '0', '1900-01-01'),
+    (3, 'Sales', 'This person is a sales rep', '0', '1900-01-01'),
+    (4, 'Roaming', 'This person is a roaming employee', '0', '1900-01-01'),
+    (5, 'Temp', 'This person is a temp', '0', '1900-01-01'),
+    (6, 'Contractor', 'This person is a contrtactor', '0', '1900-01-01'),
+    (7, 'Other', 'This person is an other type of employee', '0', '1900-01-01');
 
     
 INSERT INTO public."Roles"(
@@ -56,4 +57,6 @@ INSERT INTO public."Roles"(
     (9, 'Manager', 'Manager', '0', '1900-01-01'),
     (10, 'HR', 'Human Resources', '0', '1900-01-01'),
     (11, 'Remuneration', 'When payment needs to be done', '0', '1900-01-01'),
-    (12, 'API', 'API role', '0', '1900-01-01');
+    (12, 'SuperUser', 'Super user role', '0', '1900-01-01'),
+    (13, 'Deligate', 'Deligate user role', '0', '1900-01-01'),
+    (14, 'API', 'API role', '0', '1900-01-01');
