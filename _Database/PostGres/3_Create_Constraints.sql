ea1b50b84d7f47fc21a5eb5200ebf6ce96e75c99
roachmanza <roche.olivier@outlook.com>
2017-07-08 01:13:04 +0200

    Updated privileges

--- _Database/PostGres/3_Create_Constraints.sql
+++ _Database/PostGres/3_Create_Constraints.sql
@@ -6,5 +6,3 @@ GRANT ALL ON DATABASE "Awards_Dev" TO "Awards_Dev_User";
 
 ALTER DEFAULT PRIVILEGES
 GRANT ALL ON TABLES TO "Awards_Dev_User";
-
-ALTER TABLE public."GroupTypes" OWNER TO "Awards_Dev_User";
