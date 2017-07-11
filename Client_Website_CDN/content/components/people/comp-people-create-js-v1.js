ca6f4f32c306c27462c9bb44fd280f81d437a9ee
roachmanza <roche.olivier@outlook.com>
2017-07-12 00:10:27 +0200

    Added create person component

--- Client_Website_CDN/content/components/people/comp-people-create-js-v1.js
+++ Client_Website_CDN/content/components/people/comp-people-create-js-v1.js
@@ -1,10 +1,10 @@
-﻿function PeopleSpaViewModel(hostThisContext) {
+﻿function PeopleCreateViewModel(hostThisContext) {
     var self = this;
     self.hostContext = hostThisContext;
     var parent;
     var currentDomainLogin;
     var environment;
-    
+
     self.ApiBaseUri = ko.observable();
 
     self.WrapupReasonHasError = ko.observable(false);
@@ -13,9 +13,12 @@
     self.WrapupReasonLoading = ko.observable(false);
     self.WrapupReasonList = ko.observableArray([]);
 
-	self.apiUrl = {
-        getAllPersons: "api/v1/Persons"
-	}
+    self.apiUrl = {
+        getAllPersons: "api/v1/Persons",
+        getAllPersonTypes : "api/v1/PersonTypes",
+        getAllIdentifierTypes :  "api/v1/IdentifierTypes",
+        postPerson : "api/v1/Persons"
+    }
     //Initialize and get the nominations
     self.Initialize = function (env, parentContext, model) {
         parent = parentContext;
@@ -23,56 +26,85 @@
         self.ApiBaseUri(applicationTools.baseUrl(environment));
     };
 
-    self.OpenWrapupReason = function(){
-        var wrapupReasonId = this.id;
-        console.log("Opening "+ wrapupReasonId);
-        window.location.replace("../people/view?id="+ wrapupReasonId);
-    }
-    
-    self.EditWrapupReason = function(){
-        var wrapupReasonId = this.id;
-        console.log("Opening "+ wrapupReasonId);
-        window.location.replace("../people/edit?id="+ wrapupReasonId);
-    }
-    
-    self.CreateWrapupReason = function(){
-        console.log("Create new ");
-        window.location.replace("../people/create");
-    }
-    
+    self.OpenWrapupReasonList = function () {
+        window.location.replace("../people/list");
+    };
 
-    self.GetPeople = function () {
-        self.WrapupReasonLoading(true);
-        self.WrapupReasonList.removeAll();
+    self.personTypes = ko.observable("");
+    self.availablePersonTypes = ko.observableArray(['NONE AVAILABLE']);
+    self.firstname = ko.observable("");
+    self.lastname = ko.observable("");
+    self.email = ko.observable("");
+    self.manager = ko.observable("");
+    self.availableManagers = ko.observableArray(['NONE AVAILABLE']);
+    self.identifier = ko.observable("");
+    self.availableIdentifiers = ko.observableArray(['NONE AVAILABLE']);
+    self.identifiervalue = ko.observable();
+    self.inactive = ko.observable(true);
+    self.inactiveDate = ko.observable();
+    self.CreateWrapupReasonItem = function () {
+        var jsonObject = JSON.stringify({
+            personTypeId: self.personTypes().PersonTypeId,
+            firstname: self.firstname(),
+            lastname: self.lastname(),
+            eMail: self.email(),
+            managerPersonId: self.manager().PersonId,
+            identifierTypeId: self.identifier().IdentifierTypeId,
+            identifierValue: self.identifiervalue()
+        });
+        console.log(jsonObject)
         var url = "";
-        var headers = [applicationTools.appAuth.claimsHeader([applicationTools.appAuth.domainNameClaim("metmom\\roolivier")])];
-        url = self.ApiBaseUri() + self.apiUrl.getAllPersons;
-        console.log("Getting all people : for " + currentDomainLogin);
-        ajaxAsync.ajaxGet(self, self._populateWrapupReason, url, null, null, null, headers);
+        var headers = [applicationTools.appAuth.claimsHeader([applicationTools.appAuth.domainNameClaim(currentDomainLogin)])];
+        url = self.ApiBaseUri() + self.apiUrl.postPerson;
+        ajaxAsync.ajaxPost(self, self.OpenWrapupReasonList, url, null, jsonObject, null, headers);
     };
 
-    self._populateWrapupReason = function (result) {
-        if (result.success) {
+    //Get the person types for the dropdown
+    self.GetPersonTypes = function () {
+        self.availablePersonTypes.removeAll();
+        var url = "";
+       var headers = [applicationTools.appAuth.claimsHeader([applicationTools.appAuth.domainNameClaim(currentDomainLogin)])];
+        url = self.ApiBaseUri() + self.apiUrl.getAllPersonTypes;
+        ajaxAsync.ajaxGet(self, self._GetPersonTypes, url, null, null, null, headers);
+    };
+    self._GetPersonTypes = function (result) {
+       if (result.success) {
             var data = result.data;
-            
             for (var i = 0; i < data.length; i++) {
-                self.WrapupReasonList.push(data[i]);
+                self.availablePersonTypes.push(data[i]);
             }
-
-
-        } else {
-             if(result.errorMessage==="error"){
-                self.WrapupReasonHasError(true);
-                self.WrapupReasonError("");
-            }else{
-                self.WrapupReasonHasError(true);
-                self.WrapupReasonError(result.errorMessage);
-            }
-            
-            
         }
-        self.WrapupReasonLoading(false);
+    }
+    //Get the managers for the dropdown
+    self.GetManagers = function () {
+        self.availableManagers.removeAll();
+        var url = "";
+        var headers = [applicationTools.appAuth.claimsHeader([applicationTools.appAuth.domainNameClaim(currentDomainLogin)])];
+        url = self.ApiBaseUri() + self.apiUrl.getAllPersons;
+        ajaxAsync.ajaxGet(self, self._GetManagers, url, null, null, null, headers);
     };
-
-
+    self._GetManagers = function (result) {
+       if (result.success) {
+            var data = result.data;
+            for (var i = 0; i < data.length; i++) {
+                self.availableManagers.push(data[i]);
+            }
+        }
+    };
+    //Get the identifiers for the dropdown
+    self.GetIdentifiers = function () {
+        self.availableIdentifiers.removeAll();
+        var url = "";
+        var headers = [applicationTools.appAuth.claimsHeader([applicationTools.appAuth.domainNameClaim(currentDomainLogin)])];
+        url = self.ApiBaseUri() + self.apiUrl.getAllIdentifierTypes;
+        ajaxAsync.ajaxGet(self, self._GetIdentifiers, url, null, null, null, headers);
+    };
+    self._GetIdentifiers = function (result) {
+       if (result.success) {
+            var data = result.data;
+            for (var i = 0; i < data.length; i++) {
+                self.availableIdentifiers.push(data[i]);
+            }
+        }
+    };
 }
