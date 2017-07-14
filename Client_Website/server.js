4b601fb4e38ae9f4d615906ab7ce10cc92837808
Roche Olivier <ROlivier@mmiholdings.co.za>
2017-07-14 10:50:50 +0200

    Update server for client site , login works

--- Client_Website/server.js
+++ Client_Website/server.js
@@ -3,90 +3,141 @@ var webserver = express();
 var port = process.env.PORT || 3001;
 var bodyParser = require('body-parser');
 var path = require('path');
- //https://github.com/auth0/passport-windowsauth
+
+//https://github.com/auth0/passport-windowsauth
 var passport = require('passport');
 var WindowsStrategy = require('passport-windowsauth');
+var ensureLoggedIn = require('connect-ensure-login')
+
+var session = require('express-session');
+webserver.use(session({secret: 'keyboard cat', resave: true, saveUninitialized: false}));
 
 passport.use(new WindowsStrategy({
-  ldap: {
-    url:             'ldap://wellscordoba.wellscordobabank.com/DC=wellscordobabank,DC=com',
-    base:            'DC=wellscordobabank,DC=com',
-    bindDN:          'someAccount',
-    bindCredentials: 'andItsPass'
-  },
-  integrated:      false
-}, function(profile, done){
-  User.findOrCreate({ waId: profile.id }, function (err, user) {
-    done(err, user);
-  });
+    ldap: {
+        url: 'ldap://mmcenvdc08.metmom.mmih.biz:389/DC=metmom,DC=mmih,DC=biz',
+        base: 'DC=metmom,DC=mmih,DC=biz',
+        bindDN: 'sharepointdev',
+        bindCredentials: 'M0m3ntum123456'
+    },
+    integrated: false
+}, function (profile, done) {
+    console.log("INTO PROFILE");
+    if (profile) {
+        console.log("profile SUCCESS");
+        user = passport.serializeUser(function (profile, done) {
+            done(null, user);
+        }); //profile.user;
+        console.log("========================================= ADONE ============");
+        console.log(done);
+        console.log(profile)
+        //debugger;
+        return done(null, user);
+    } else {
+        console.log("profile ERROR");
+        return done(null, false);
+    }
 }));
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
 
 //middleware section
 //gets executed on each request
 webserver.use(bodyParser.urlencoded({ extended: true }));
 webserver.use(bodyParser.json());
+webserver.use(passport.initialize());
+webserver.use(passport.session());
 
-webserver.post('/login',
-  passport.authenticate('WindowsAuthentication', {
-                                  successRedirect: '/home/',
-                                  failureRedirect: '/login',
-                                  failureFlash:    true })
-);
+//Post for the login form
+webserver.post('/login', passport.authenticate('WindowsAuthentication', {
+    successRedirect: '/home',
+    failureRedirect: '/testerror',
+    failureFlash: true,
+    session: true
+}));
+
 //Client website navigational pages
 //Each root patch maps to a relevant folder
-webserver.get('/test',  passport.authenticate('WindowsAuthentication'), function (req, res){
-    if(err){
-    console.log("error" + err.message);
-    }else{
-        res.sendFile(path.join(__dirname + '/client/index.html'));
-    }    
-});
-webserver.get('/express-passport',  passport.authenticate('WindowsAuthentication'), function (req, res){
-    res.json(req.user);
-});
-webserver.get('/', function(req, res) {
+// webserver.get('/testsuccess', function (req, res) {
+//      console.log("INTO testsuccess.......");
+//     isAuthenticated(req, res);   
+//     res.sendFile(path.join(__dirname + '/client/index.html'));
+// });
+//TODO: removed ensureLoggedIn logged in
+webserver.get('/testerror',
+    function (req, res) {
+        console.log("INTO testerror.......");
+        res.sendFile(path.join(__dirname + '/client/error.html'));
+    });
+webserver.get('/login', function (req, res) {
     res.sendFile(path.join(__dirname + '/client/login.html'));
 });
-webserver.get('/login', function(req, res) {
+
+
+webserver.get('/', function (req, res) {
+    isAuthenticated(req, res);
     res.sendFile(path.join(__dirname + '/client/login.html'));
 });
-webserver.get('/home/', function(req, res) {
+webserver.get('/home/', function (req, res) {
+    isAuthenticated(req, res);
     res.sendFile(path.join(__dirname + '/client/index.html'));
 });
-webserver.get('/people/', function(req, res) {
+
+webserver.get('/people/', function (req, res) {
+    isAuthenticated(req, res);
     res.sendFile(path.join(__dirname + '/client/pages/people/people.html'));
 });
-webserver.get('/people/list', function(req, res) {
+webserver.get('/people/list', function (req, res) {
     res.sendFile(path.join(__dirname + '/client/pages/people/peopleList.html'));
 });
-webserver.get('/people/create', function(req, res) {
+webserver.get('/people/create', function (req, res) {
     res.sendFile(path.join(__dirname + '/client/pages/people/peoplecreate.html'));
 });
-webserver.get('/people/view', function(req, res) {
+webserver.get('/people/view', function (req, res) {
     res.sendFile(path.join(__dirname + '/client/pages/people/peopleview.html'));
 });
-webserver.get('/people/edit', function(req, res) {
+webserver.get('/people/edit', function (req, res) {
     res.sendFile(path.join(__dirname + '/client/pages/people/peopleedit.html'));
 });
-webserver.get('/people/search', function(req, res) {
+webserver.get('/people/search', function (req, res) {
     res.sendFile(path.join(__dirname + '/client/pages/people/peopleSearchList.html'));
 });
 
 
-webserver.get('/programmes/', function(req, res) {
+webserver.get('/programmes/', function (req, res) {
+    isAuthenticated(req, res);
     res.sendFile(path.join(__dirname + '/client/pages/programsSpa.html'));
 });
-webserver.get('/ascriptions/', function(req, res) {
+webserver.get('/ascriptions/', function (req, res) {
+    isAuthenticated(req, res);
     res.sendFile(path.join(__dirname + '/client/pages/ascriptionsSpa.html'));
 });
-webserver.get('/about/', function(req, res) {
+webserver.get('/about/', function (req, res) {
     res.sendFile(path.join(__dirname + '/client/info/about.html'));
 });
-webserver.get('/contactus/', function(req, res) {
+webserver.get('/contactus/', function (req, res) {
     res.sendFile(path.join(__dirname + '/client/info/contactus.html'));
 });
+
 
 
 //Listen on port number
 webserver.listen(port);
 console.log('Award site server started on: ' + port);
+
+
+var isAuthenticated = function (req, res, next) {
+    if (req.user) {
+        console.log("isAuthenticated : " + req.user);
+        // console.log("authed" + req.user.displayName);
+        return next;
+    } else {
+        res.sendFile(path.join(__dirname + '/client/login.html'));
+        //   return res.status(401).json({
+        //     error: 'User not authenticated'
+        //   })
+    }
+}
